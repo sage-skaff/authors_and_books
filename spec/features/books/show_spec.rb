dd59d6f68fef291c3d522d288fcd2d3af096e6db
Sage Skaff <skaffsage@gmail.com>
2022-07-08 09:02:31 -0700

    Add tests for books show page

---
+++ spec/features/books/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Books Show Page' do
+  it 'lists books attributes' do
+    author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
+    author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
+    book = Book.create!(title: 'The Hobbit', pages: 310, series: false, author_id: author.id)
+    book2 = Book.create!(title: 'Dune', pages: 412, series: true, author_id: author2.id)
+
+    visit "/books/#{book.id}"
+    expect(page).to have_content('The Hobbit')
+    expect(page).to have_content('310')
+    expect(page).to have_content('Series: false')
+    expect(page).to have_content('J.R.R. Tolkien')
+    expect(page).to_not have_content('Dune')
+    expect(page).to_not have_content('412')
+    expect(page).to_not have_content('Series: true')
+    expect(page).to_not have_content('Frank Herbert')
+  end
+end
