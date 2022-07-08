894313b18399949a9f36d4f2b46b7fcf5a8c1533
Sage Skaff <skaffsage@gmail.com>
2022-07-08 16:53:20 -0700

    Add test for most recently created authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Author, type: :model do
   describe 'relationships' do
     it { should have_many :books }
   end
+
+  describe 'class methods' do
+    it 'can list authors by most recently created' do
+      author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
+      author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
+      author3 = Author.create!(name: 'Isaac Asimov', age: 72, living: false)
+
+      expect(Author.most_recently_created).to eq([author, author2, author3])
+    end
+  end
 end
