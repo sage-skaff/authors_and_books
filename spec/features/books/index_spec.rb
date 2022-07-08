35ff083601b02c7cb45f0a6e63c3501a0c4a2efe
Sage Skaff <skaffsage@gmail.com>
2022-07-08 08:40:53 -0700

    Add tests for books index page

---
+++ spec/features/books/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Books Index Page' do
+  it 'lists names of all the books and their attributes' do
+    author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
+    author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
+    book = Book.create!(title: 'The Hobbit', pages: 310, series: false, author_id: author.id)
+    book2 = Book.create!(title: 'Dune', pages: 412, series: true, author_id: author2.id)
+
+    visit '/books'
+    within '#index-0' do
+      expect(page).to have_content('The Hobbit')
+      expect(page).to have_content('310')
+      expect(page).to have_content('Single')
+      expect(page).to have_content('J.R.R. Tolkien')
+      expect(page).to_not have_content('Dune')
+      expect(page).to_not have_content('412')
+      expect(page).to_not have_content('Series')
+      expect(page).to_not have_content('Frank Herbert')
+    end
+
+    within '#index-1' do
+      expect(page).to have_content('Dune')
+      expect(page).to have_content('412')
+      expect(page).to have_content('Series')
+      expect(page).to have_content('Frank Herbert')
+      expect(page).to_not have_content('The Hobbit')
+      expect(page).to_not have_content('310')
+      expect(page).to_not have_content('Single')
+      expect(page).to_not have_content('J.R.R. Tolkien')
+    end
+  end
+end
