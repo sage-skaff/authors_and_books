1220d1328a209aef28dd923a875bd8ed6eaecbe0
Sage Skaff <skaffsage@gmail.com>
2022-07-13 14:10:33 -0700

    Add all series to books index

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   validates_presence_of :title, :pages
   validates :series, inclusion: [true, false]
   belongs_to :author
+
+  def self.all_series
+    all.where(series: true)
+  end
 end
