65a4b6cfa4507701f1317a9164ec7d8bb7f2f801
Sage Skaff <skaffsage@gmail.com>
2022-07-13 14:06:38 -0700

    Add test for books in a series

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe 'Books Index Page' do
   it 'lists names of all the books and their attributes' do
     author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
     author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
-    book = Book.create!(title: 'The Hobbit', pages: 310, series: false, author_id: author.id)
+    book = Book.create!(title: 'The Fellowship of the Ring', pages: 423, series: true, author_id: author.id)
     book2 = Book.create!(title: 'Dune', pages: 412, series: true, author_id: author2.id)
 
     visit '/books'
     within '#index-0' do
-      expect(page).to have_content('The Hobbit')
-      expect(page).to have_content('310')
-      expect(page).to have_content('Series: false')
+      expect(page).to have_content('The Fellowship of the Ring')
+      expect(page).to have_content('423')
+      expect(page).to have_content('Series: true')
       expect(page).to have_content('J.R.R. Tolkien')
       expect(page).to_not have_content('Dune')
       expect(page).to_not have_content('412')
-      expect(page).to_not have_content('Series: true')
       expect(page).to_not have_content('Frank Herbert')
     end
 
@@ -26,7 +25,6 @@ RSpec.describe 'Books Index Page' do
       expect(page).to have_content('Frank Herbert')
       expect(page).to_not have_content('The Hobbit')
       expect(page).to_not have_content('310')
-      expect(page).to_not have_content('Series: false')
       expect(page).to_not have_content('J.R.R. Tolkien')
     end
   end
@@ -36,5 +34,19 @@ RSpec.describe 'Books Index Page' do
 
     expect(page).to have_link('Authors Index')
     expect(page).to have_link('Books Index')
+  end
+
+  it 'only shows books that are in a series' do
+    author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
+    author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
+    book = Book.create!(title: 'The Hobbit', pages: 310, series: false, author_id: author.id)
+    book2 = Book.create!(title: 'Dune', pages: 412, series: true, author_id: author2.id)
+    book3 = Book.create!(title: 'The Fellowship of the Ring', pages: 423, series: true, author_id: author.id)
+
+    visit '/books'
+
+    expect(page).to have_content('Dune')
+    expect(page).to have_content('The Fellowship of the Ring')
+    expect(page).to_not have_content('The Hobbit')
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Book, type: :model do
   describe 'relationships' do
     it { should belong_to :author }
   end
+
+  describe 'class methods' do
+    it 'can list only books that are in a series' do
+      author = Author.create!(name: 'J.R.R. Tolkien', age: 81, living: false)
+      author2 = Author.create!(name: 'Frank Herbert', age: 65, living: true)
+      book = Book.create!(title: 'The Hobbit', pages: 310, series: false, author_id: author.id)
+      book2 = Book.create!(title: 'Dune', pages: 412, series: true, author_id: author2.id)
+      book3 = Book.create!(title: 'The Fellowship of the Ring', pages: 423, series: true, author_id: author.id)
+
+      expect(Book.all_series).to eq([book2, book3])
+    end
+  end
 end
